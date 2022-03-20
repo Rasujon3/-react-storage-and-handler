5dfdac43dacac17f513ac759bea5891433e12b1e
Rasujon3 <rasujon3@gmail.com>
2022-03-21 02:33:32 +0600

    Kono ekta event handler k parameter soho add krte chaw, => arrow function banaye func k call kore parameter pass krte hobe

--- src/components/Cosmatic/Cosmatic.js
+++ src/components/Cosmatic/Cosmatic.js
@@ -1,15 +1,26 @@
 import React from "react";
+import "./Cosmatic.css";
 
 const Cosmatic = (props) => {
-  console.log(props.cosmatic);
+  //   console.log(props.cosmatic);
   const { name, price, id } = props.cosmatic;
+
+  const addToCart = (id) => {
+    console.log("item added", id);
+  };
+
+  const addtoCartWithParam = () => {
+    addToCart(id);
+  };
   return (
-    <div>
+    <div className="product">
       <h2>Buy this: {name}</h2>
       <p>Only for: ${price}</p>
       <p>
         <small>it ha id : {id}</small>
       </p>
+      <button onClick={addtoCartWithParam}>Add to Cart</button>
+      <button onClick={() => addToCart(id)}>Add to Cart: shortcut</button>
     </div>
   );
 };
--- src/components/Cosmatics/Cosmatics.js
+++ src/components/Cosmatics/Cosmatics.js
@@ -1,39 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cosmatic from "../Cosmatic/Cosmatic";
 
 const Cosmatics = () => {
-  const cosmatics = [
-    {
-      id: "62377c4521e8974b50cc73b4",
-      price: 543,
-      eyeColor: "blue",
-      name: "Drake Maldonado",
-    },
-    {
-      id: "62377c4539d14188ce83b0dc",
-      price: 443,
-      eyeColor: "brown",
-      name: "Potts Flynn",
-    },
-    {
-      id: "62377c45661e9371a8985859",
-      price: 153,
-      eyeColor: "blue",
-      name: "Celina Wells",
-    },
-    {
-      id: "62377c45300bd5d99bf46362",
-      price: 347,
-      eyeColor: "blue",
-      name: "Wooten Hughes",
-    },
-    {
-      id: "62377c454e820c36ba85d69a",
-      price: 226,
-      eyeColor: "brown",
-      name: "Leon Peters",
-    },
-  ];
+  const [cosmatics, setCosmatics] = useState([]);
+  useEffect(() => {
+    const url = `data.json`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCosmatics(data));
+  }, []);
   return (
     <div>
       <h1>Welcome to my cosmetics store</h1>
