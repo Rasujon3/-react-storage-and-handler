b32bfa733dd0700a333d4e9fbe9f7eabc244f9b8
Rasujon3 <rasujon3@gmail.com>
2022-03-21 01:13:47 +0600

    json generator

--- src/components/Cosmatics/Cosmatics.js
+++ src/components/Cosmatics/Cosmatics.js
@@ -3,11 +3,36 @@ import Cosmatic from "../Cosmatic/Cosmatic";
 
 const Cosmatics = () => {
   const cosmatics = [
-    { id: 1, name: "Alta", price: 100 },
-    { id: 2, name: "palis", price: 300 },
-    { id: 3, name: "malis", price: 300 },
-    { id: 4, name: "balis", price: 400 },
-    { id: 5, name: "nalis", price: 500 },
+    {
+      id: "62377c4521e8974b50cc73b4",
+      price: 543,
+      eyeColor: "blue",
+      name: "Drake Maldonado",
+    },
+    {
+      id: "62377c4539d14188ce83b0dc",
+      price: 443,
+      eyeColor: "brown",
+      name: "Potts Flynn",
+    },
+    {
+      id: "62377c45661e9371a8985859",
+      price: 153,
+      eyeColor: "blue",
+      name: "Celina Wells",
+    },
+    {
+      id: "62377c45300bd5d99bf46362",
+      price: 347,
+      eyeColor: "blue",
+      name: "Wooten Hughes",
+    },
+    {
+      id: "62377c454e820c36ba85d69a",
+      price: 226,
+      eyeColor: "brown",
+      name: "Leon Peters",
+    },
   ];
   return (
     <div>
