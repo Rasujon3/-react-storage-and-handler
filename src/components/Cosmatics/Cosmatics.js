41bfe177bf5b2cf4bb0d4b6b4fc7b391eaa4e16d
Rasujon3 <rasujon3@gmail.com>
2022-03-21 03:58:56 +0600

    47_5-8 Module Summary and simple intro to array reduce

--- src/components/Cosmatics/Cosmatics.js
+++ src/components/Cosmatics/Cosmatics.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cosmatic from "../Cosmatic/Cosmatic";
+import { getTotal } from "../utilities/calculate";
 
 const Cosmatics = () => {
   const [cosmatics, setCosmatics] = useState([]);
@@ -9,9 +10,11 @@ const Cosmatics = () => {
       .then((res) => res.json())
       .then((data) => setCosmatics(data));
   }, []);
+  const total = getTotal(cosmatics);
   return (
     <div>
       <h1>Welcome to my cosmetics store</h1>
+      <p>Money Needed: {total} tk</p>
       {cosmatics.map((cosmatic) => (
         <Cosmatic key={cosmatic.id} cosmatic={cosmatic} />
       ))}
